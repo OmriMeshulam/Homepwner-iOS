57c1cb16ad4768d6d5deb80ff596f27ff1ecb7b8
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-04 17:26:17 -0500

    Specifying row height, font size, and background image. Chapter 8 Gold challenge.

---
+++ Homepwner_Gold/Homepwner/OGMItemsViewController.h
@@ -0,0 +1,15 @@
+//
+//  OGMItemsViewController.h
+//  Homepwner
+//
+//  Created by Omri Meshulam on 3/2/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OGMItemsViewController : UITableViewController
+
+@property(nonatomic, readwrite, retain) UIView *backgroundView;
+
+@end
