9aa2eee52e2ac84e51907f2ab3f3e1019d247829
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-05 22:15:35 -0500

    Renaming the Delete Button. Chapter 9 Bronze Challenge.

---
+++ Homepwner_Bronze/Chapter 9/Homepwner/OGMItemsViewController.m
@@ -0,0 +1,153 @@
+//
+//  OGMItemsViewController.m
+//  Homepwner
+//
+//  Created by Omri Meshulam on 3/2/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import "OGMItemsViewController.h"
+#import "OGMItemStore.h"
+#import "OGMItem.h"
+
+@interface OGMItemsViewController ()
+
+@property (nonatomic, strong) IBOutlet UIView *headerView;
+
+@end
+
+@implementation OGMItemsViewController
+
+- (instancetype)init
+{
+    // Call to superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    
+    if(self){
+        
+    }
+    
+    return self;
+}
+
+// Designated initializer
+// Overriding it to call our inits
+- (instancetype) initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+// Required method, number of rows
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[OGMItemStore sharedStore] allItems] count];
+}
+
+// Required method, row data
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Get a new or recycled cell
+    // implicility creating tableViewCell by Apple to get the benefits of the reuse identifier
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    
+    // Setting the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableView
+    NSArray *items = [[OGMItemStore sharedStore] allItems];
+    OGMItem *item = items[indexPath.row];
+
+    cell.textLabel.text = [item description];
+    
+    return cell;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Telling the table view which kind of cell it should instantiate if there are no cells in the reuse pool
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
+    
+}
+
+- (IBAction)addNewItem:(id)sender
+{
+    // Creating a new OGMItem and adding it to the store
+    OGMItem *newItem = [[OGMItemStore sharedStore] createItem];
+    
+    // Figuring out where that item is in the array
+    NSInteger lastRow = [[[OGMItemStore sharedStore] allItems] indexOfObject:newItem];
+    
+    // Make a new index path for the 0th section, last row
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    
+    // Inserting this row into the table
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+}
+
+- (IBAction)toggleEditingMode:(id)sender
+{
+    // If you are already currently in editing mode
+    if(self.isEditing){
+        // Change the text of the button to inform user of state
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        
+        // Turn off editing mode
+        [self setEditing:NO animated:YES];
+    }else{
+        //Change the text of button to inform user of state
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        
+        // Enter editing mode
+        [self setEditing:YES animated:YES];
+    }
+}
+
+- (UIView *)headerView
+{
+    // If the headerView has not been loaded yet
+    if(!_headerView){
+        // Load HeadView.xib
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    
+    return _headerView;
+    
+}
+
+// For deleting a row
+- (void)tableView:(UITableView *)tableView
+    commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+    forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // If the table view is asking to commit a delete command
+    if(editingStyle == UITableViewCellEditingStyleDelete){
+        NSArray *items = [[OGMItemStore sharedStore] allItems];
+        OGMItem *item = items[indexPath.row];
+        [[OGMItemStore sharedStore] removeItem:item];
+        
+        // Also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:YES];
+    }
+}
+
+// For moving a row
+- (void) tableView:(UITableView *)tableView
+    moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+        toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[OGMItemStore sharedStore] moveItemAtIdex:sourceIndexPath.row
+                                       toIndex:destinationIndexPath.row];
+}
+
+// Renaming Delete button text to Remove
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return @"Remove";
+}
+
+
+@end
