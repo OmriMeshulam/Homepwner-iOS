7da1cef9e22835e6a16b6441afcaba7dbbbe5a54
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-07 16:21:21 -0500

    Displaying a number pad, change in xib attributes inspector.

---
+++ Homepwner_Bronze_Challenge/Chapter 10/OGMDetailViewController.m
@@ -0,0 +1,65 @@
+//
+//  OGMDetailViewController.m
+//  Homepwner
+//
+//  Created by Omri Meshulam on 3/6/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import "OGMDetailViewController.h"
+#import "OGMItem.h"
+
+@interface OGMDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+
+@end
+
+@implementation OGMDetailViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    OGMItem *item = self.item;
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d", item.valueInDollars];
+    
+    // NSDateFormatter turns date into a simple date string
+    static NSDateFormatter *dateFormatter = nil;
+    if(!dateFormatter){
+        dateFormatter = [[NSDateFormatter alloc]init];
+        dateFormatter.dateStyle = NSDateIntervalFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    // Use filtered NSDate object to set dateLabel contents
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    // Clear first responder
+    [self.view endEditing:YES];
+    
+    // Saving changes to item information
+    OGMItem *item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialNumberField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+}
+
+- (void)setItem:(OGMItem *)item
+{
+    _item = item;
+    self.navigationItem.title = item.itemName;
+}
+
+@end
