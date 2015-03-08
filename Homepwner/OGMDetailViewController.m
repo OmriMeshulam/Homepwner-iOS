fa76f0f293a6ef4f3b56dbf50f4e40d5deba1ffe
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-07 20:10:09 -0500

    UIToolbar, displaying images, taking pictures and UIImagePickerController.

--- Homepwner/OGMDetailViewController.m
+++ Homepwner/OGMDetailViewController.m
@@ -10,11 +10,15 @@
 #import "OGMItem.h"
 
 @interface OGMDetailViewController ()
+    <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UITextField *valueField;
 @property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
 
 @end
 
@@ -60,6 +64,38 @@
 {
     _item = item;
     self.navigationItem.title = item.itemName;
+}
+
+- (IBAction)takePicture:(id)sender {
+    
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    
+    // If the device has a camera, take a picture, otherwise
+    // just pick from photo library
+    if ([UIImagePickerController
+         isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }else{
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    imagePicker.delegate = self;
+    
+    //Place imagePicker on the screen
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // Get picked image from the info dictionary
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    
+    // Put that image onto the screen in our image view
+    self.imageView.image = image;
+    
+    // Take the image picker off the screen
+    // You must call this dismiss method
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
