b87009b8bfe943d4f17b329cf9fae01bfb480ce0
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-08 16:56:35 -0400

    ImagePicker crosshair image overlay. Chapter 11 Gold Challenge: Camera Overlay.

---
+++ Homepwner_Gold_Challenge/Chapter 11/OGMDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  OGMDetailViewController.h
+//  Homepwner
+//
+//  Created by Omri Meshulam on 3/6/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OGMItem;
+
+@interface OGMDetailViewController : UIViewController
+
+@property (nonatomic, strong) OGMItem *item;
+
+@end
