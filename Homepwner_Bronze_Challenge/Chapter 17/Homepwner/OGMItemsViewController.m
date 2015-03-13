25f0ed23bbcb635079b97de689d9dc0aa623e4f7
Omri Meshulam <omri.meshulam@gmail.com>
2015-03-13 19:29:10 -0400

    Ch 17 Bronze Challenge: Another Thread-Safe Singleton, Updating Itemstore class singleton to also use dispatch_once()

---
+++ Homepwner_Bronze_Challenge/Chapter 17/Homepwner/OGMItemsViewController.m
@@ -0,0 +1,147 @@
+//
+//  OGMItemsViewController.m
+//  Homepwner
+//
+//  Created by Omri Meshulam on 3/2/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import "OGMItemsViewController.h"
+#import "OGMItemStore.h"
+#import "OGMItem.h"
+#import "OGMDetailViewController.h"
+
+@interface OGMItemsViewController ()
+
+@end
+
+@implementation OGMItemsViewController
+
+- (instancetype)init
+{
+    // Call to superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    
+    if(self){
+        UINavigationItem *navItem = self.navigationItem;
+        navItem.title = @"Homepwner";
+        
+        // Creating a new bar button item that will send
+        // addNewItem: to OGMItemsViewController
+        UIBarButtonItem *bbi = [[UIBarButtonItem alloc]
+                                initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                target:self
+                                action:@selector(addNewItem:)];
+        // Setting this bar button item as the right item in the navigationItem
+        navItem.rightBarButtonItem = bbi;
+        
+        navItem.leftBarButtonItem = self.editButtonItem;
+    }
+    
+    return self;
+}
+
+// Designated initializer
+// Overriding it to call our inits
+- (instancetype) initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+// Required method, number of rows
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[OGMItemStore sharedStore] allItems] count];
+}
+
+// Required method, row data
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Get a new or recycled cell
+    // implicility creating tableViewCell by Apple to get the benefits of the reuse identifier
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    
+    // Setting the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableView
+    NSArray *items = [[OGMItemStore sharedStore] allItems];
+    OGMItem *item = items[indexPath.row];
+
+    cell.textLabel.text = [item description];
+    
+    return cell;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Telling the table view which kind of cell it should instantiate if there are no cells in the reuse pool
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.tableView reloadData];
+}
+
+- (IBAction)addNewItem:(id)sender
+{
+    // Creating a new OGMItem and adding it to the store
+    OGMItem *newItem = [[OGMItemStore sharedStore] createItem];
+    
+    OGMDetailViewController *detailViewController = [[OGMDetailViewController alloc]initForNewItem:YES];
+    detailViewController.item = newItem;
+    detailViewController.dismissBlock = ^{
+        [self.tableView reloadData];
+    };
+    UINavigationController *navController = [[UINavigationController alloc]initWithRootViewController:detailViewController];
+    
+    navController.modalPresentationStyle = UIModalPresentationFormSheet;
+    
+    [self presentViewController:navController animated:YES completion:nil];
+}
+
+// For deleting a row
+- (void)tableView:(UITableView *)tableView
+    commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+    forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // If the table view is asking to commit a delete command
+    if(editingStyle == UITableViewCellEditingStyleDelete){
+        NSArray *items = [[OGMItemStore sharedStore] allItems];
+        OGMItem *item = items[indexPath.row];
+        [[OGMItemStore sharedStore] removeItem:item];
+        
+        // Also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:YES];
+    }
+}
+
+// For moving a row
+- (void) tableView:(UITableView *)tableView
+    moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+        toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[OGMItemStore sharedStore] moveItemAtIdex:sourceIndexPath.row
+                                       toIndex:destinationIndexPath.row];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    OGMDetailViewController *detailviewController = [[OGMDetailViewController alloc] initForNewItem:NO];
+    
+    NSArray *items = [[OGMItemStore sharedStore] allItems];
+    OGMItem *selectedItem = items[indexPath.row];
+    
+    // Giving detail view controller a pointer to the item object in row
+    detailviewController.item = selectedItem;
+    
+    // Push it onto the top of the navigation controllers stack
+    [self.navigationController pushViewController:detailviewController animated:YES];
+}
+
+@end
---
+++ Homepwner_Bronze_Challenge/Chapter 17/OGMDetailViewController.m
@@ -0,0 +1,270 @@
+//
+//  OGMDetailViewController.m
+//  Homepwner
+//
+//  Created by Omri Meshulam on 3/6/15.
+//  Copyright (c) 2015 Omri Meshulam. All rights reserved.
+//
+
+#import "OGMDetailViewController.h"
+#import "OGMItem.h"
+#import "OGMImageStore.h"
+#import "OGMItemStore.h"
+
+@interface OGMDetailViewController ()
+    <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate, UIPopoverControllerDelegate>
+
+@property (nonatomic, strong)UIPopoverController *imagePickerPopover;
+
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *cameraButton;
+
+@end
+
+@implementation OGMDetailViewController
+
+- (instancetype)initForNewItem:(BOOL)isNew
+{
+    self = [super initWithNibName:nil bundle:nil];
+    
+    if(self){
+        if(isNew){
+            UIBarButtonItem *doneItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                                     target:self
+                                                                                     action:@selector(save:)];
+            self.navigationItem.rightBarButtonItem = doneItem;
+            
+            UIBarButtonItem *cancelItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
+                                                                                       target:self
+                                                                                       action:@selector(cancel:)];
+            self.navigationItem.leftBarButtonItem = cancelItem;
+        }
+    }
+    return self;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    @throw [NSException exceptionWithName:@"Wrong initializer"
+                                   reason:@"Use initForNewItem:"
+                                 userInfo:nil];
+    return nil;
+}
+
+- (IBAction)backgroundTapped:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImageView *iv = [[UIImageView alloc]initWithImage:nil];
+    
+    // The contentMode of the image view in the XIB was Aspect fit:
+    iv.contentMode = UIViewContentModeScaleAspectFit;
+    
+    // Do not produce a translated contraint for this view
+    iv.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    // The image view was a subview of the view
+    [self.view addSubview:iv];
+    
+    // The image view was pointed to by the imageView property
+    self.imageView = iv;
+    
+    // Set the vertical priorities to be less than those of the other views
+    [self.imageView setContentHuggingPriority:200 forAxis:UILayoutConstraintAxisVertical];
+    [self.imageView setContentCompressionResistancePriority:700 forAxis:UILayoutConstraintAxisVertical];
+    
+    NSDictionary *nameMap = @{@"imageView" : self.imageView,
+                              @"dateLabel" : self.dateLabel,
+                              @"toolbar" : self.toolbar};
+    
+    // imageView is 0 pts from superview at left and right edges
+    NSArray *horizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[imageView]-0-|"
+                                                                             options:0
+                                                                             metrics:nil
+                                                                               views:nameMap];
+    // imageView is 8 pts from dateLabel at its top edge
+    // ... and 8 pts from toolbar at its bottom edge
+    NSArray *verticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[dateLabel]-[imageView]-[toolbar]"
+                                                                           options:0
+                                                                           metrics:nil
+                                                                             views:nameMap];
+    [self.view addConstraints:horizontalConstraints];
+    [self.view addConstraints:verticalConstraints];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    UIInterfaceOrientation io = [[UIApplication sharedApplication]statusBarOrientation];
+    [self prepareViewsForOrientation:io];
+    
+    OGMItem *item = self.item;
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d", item.valueInDollars];
+    
+    // NSDateFormatter turns date into a simple date string
+    static NSDateFormatter *dateFormatter = nil;
+    if(!dateFormatter){
+        dateFormatter = [[NSDateFormatter alloc]init];
+        dateFormatter.dateStyle = NSDateIntervalFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    // Use filtered NSDate object to set dateLabel contents
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+    
+    NSString *imageKey = self.item.itemKey;
+    
+    // Getting the image for its image key from the image store
+    UIImage *imageToDisplay = [[OGMImageStore sharedStore] imageForKey:imageKey];
+    
+    // Using that image to put on the screen in the imageView
+    self.imageView.image = imageToDisplay;
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    // Clear first responder
+    [self.view endEditing:YES];
+    
+    // Saving changes to item information
+    OGMItem *item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialNumberField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+}
+
+- (void)setItem:(OGMItem *)item
+{
+    _item = item;
+    self.navigationItem.title = item.itemName;
+}
+
+- (IBAction)takePicture:(id)sender {
+    
+    if([self.imagePickerPopover isPopoverVisible]){
+        // If the popover is already up, get rid of it
+        [self.imagePickerPopover dismissPopoverAnimated:YES];
+        self.imagePickerPopover = nil;
+        return;
+    }
+    
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    
+    // If the device has a camera, take a picture, otherwise
+    // just pick from photo library
+    if ([UIImagePickerController
+         isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }else{
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    imagePicker.delegate = self;
+    
+    // Place imagePicker on the screen
+    // Check for iPad device before instantiating the popover controller
+    if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad){
+        // Create new popover controller that will display the imagePicker
+        self.imagePickerPopover = [[UIPopoverController alloc]initWithContentViewController:imagePicker];
+        self.imagePickerPopover.delegate = self;
+        
+        // Display the popover controller;
+        // Sender is the camera bar button item
+        [self.imagePickerPopover presentPopoverFromBarButtonItem:sender
+                                        permittedArrowDirections:UIPopoverArrowDirectionAny
+                                                        animated:YES];
+    }else{
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    }
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // Get picked image from the info dictionary
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    
+    // Storing the image in the OGMImageStore for this key
+    [[OGMImageStore sharedStore] setImage:image forKey:self.item.itemKey];
+    
+    // Put that image onto the screen in our image view
+    self.imageView.image = image;
+    
+    // Take the image picker off the screen
+    // You must call this dismiss method
+    
+    // Do I have a popover?
+    if(self.imagePickerPopover){
+        // Dismiss it
+        [self.imagePickerPopover dismissPopoverAnimated:YES];
+        self.imagePickerPopover = nil;
+    }else{
+        // Dismiss the modal image picker
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)prepareViewsForOrientation:(UIInterfaceOrientation)orientation
+{
+    // Is it an iPad? No preparation necessary
+    if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+        return;
+    }
+    
+    // Is it in landscape mode?
+    if(UIInterfaceOrientationIsLandscape(orientation)){
+        self.imageView.hidden = YES;
+        self.cameraButton.enabled = NO;
+    }else{
+        self.imageView.hidden = NO;
+        self.cameraButton.enabled = YES;
+    }
+}
+
+- (void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    [self prepareViewsForOrientation:toInterfaceOrientation];
+}
+
+- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController
+{
+    NSLog(@"User dismissed popover");
+    self.imagePickerPopover = nil;
+}
+
+- (void)save:(id)sender
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:self.dismissBlock];
+}
+
+- (void)cancel:(id)sender
+{
+    // If the user cancelled, then remove the OGMItem from the store
+    [[OGMItemStore sharedStore] removeItem:self.item];
+    
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:self.dismissBlock];
+}
+
+@end
